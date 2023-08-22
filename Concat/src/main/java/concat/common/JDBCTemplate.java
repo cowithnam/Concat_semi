1e85cca4868023b6e49ffdc42f2c3ff3edaf61c2
민병진 <lebecadell@gmail.com>
2023-08-22 12:06:20 +0900

    Main branch

---
+++ Concat/src/main/java/concat/common/JDBCTemplate.java
@@ -0,0 +1,16 @@
+package concat.common;
+
+import java.sql.Connection;
+import java.util.Properties;
+
+public class JDBCTemplate {
+	public Connection getConnection() {
+		Connection conn = null;
+		
+		Properties prop = new Properties();
+		
+		String filePath = JDBCTemplate.class.getResource("/db/driver/driver.properties").getPath();
+		
+	}
+	
+}
